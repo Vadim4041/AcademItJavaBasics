dcdf2d7fe12c8f6480cceef151eb932cefba2ce1
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-17 15:00:14 +0700

    renamed homework 1 file

--- Homework/src/Homework1/Task1CreateProject.java
+++ Homework/src/Homework1/Task1CreateProject.java
@@ -1,6 +1,6 @@
 package Homework1;
 
-public class Task1 {
+public class Task1CreateProject {
     public static void main(String[] args) {
         int a = 5 * 5 + 18;
         int b = 48 - a * 15 + 46;
