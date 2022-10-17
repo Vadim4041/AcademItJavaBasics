266e4aed5e3e4457964022123b42a5cf26d3c12a
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-17 14:06:11 +0700

    added homework 1

---
+++ Homework/src/Homework1/Task1.java
@@ -0,0 +1,10 @@
+package Homework1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 5 * 5 + 18;
+        int b = 48 - a * 15 + 46;
+
+        System.out.println(b);
+    }
+}
