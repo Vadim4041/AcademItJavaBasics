6a1c0e098b4b26e8c6c3ba7fd3467fabd66f2a18
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-19 09:13:26 +0700

    updated Circle.java

--- Homework/src/Homework2/Circle.java
+++ Homework/src/Homework2/Circle.java
@@ -20,17 +20,17 @@ public class Circle {
     public static void main(String[] args) {
         double givenCircleRadius = 5;
         double circleArea = Math.pow(givenCircleRadius, 2) * Math.PI;
-        System.out.println("Площадь круга заданного радиуса равна: " + circleArea);
+        System.out.println("Площадь круга заданного радиуса равна:" + circleArea);
         double circumference = 2 * Math.PI * givenCircleRadius;
-        System.out.println("Длина окружности заданного радиуса равна: " + circumference);
+        System.out.println("Длина окружности заданного радиуса равна:" + circumference);
 
         double givenCircleArea = 78.53981633974483;
         double radius = Math.sqrt(givenCircleArea / Math.PI);
-        System.out.println("Радиус окружности заданной площади равен: " + radius);
+        System.out.println("Радиус окружности заданной площади равен:" + radius);
 
         double sectorAngle = 45;
         double sectorRadius = 5;
         double sectorArea = Math.toRadians(sectorAngle) * Math.pow(sectorRadius, 2) / 2;
-        System.out.println("Площадь сектора круга с заданным радиусом и углом равна: " + sectorArea);
+        System.out.println("Площадь сектора круга с заданным радиусом и углом равна:" + sectorArea);
     }
 }
