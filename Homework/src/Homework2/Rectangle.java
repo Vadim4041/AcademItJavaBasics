0008938eb9869e24f974055686a0bbed7b1bd3b1
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-19 09:15:16 +0700

    updated Rectangle.java

--- Homework/src/Homework2/Rectangle.java
+++ Homework/src/Homework2/Rectangle.java
@@ -17,15 +17,18 @@ public class Rectangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите ширину прямоугольника: ");
+        System.out.println("Введите ширину прямоугольника:");
+
         double width = scanner.nextDouble();
-        System.out.println("Введите длину прямоугольника: ");
+
+        System.out.println("Введите длину прямоугольника:");
+
         double length = scanner.nextDouble();
 
-        double perimeter = width + length;
+        double perimeter = 2 * (width + length);
         double area = width * length;
 
-        System.out.printf("Введен прямоугольник шириной %.02f и длиной %.02f. Периметр такого прямоугольника равен %.02f, а площадь равна %.02f.", width, length, perimeter, area);
-
+        System.out.printf("Введен прямоугольник шириной %.2f и длиной %.2f. " +
+                "Периметр такого прямоугольника равен %.2f, а площадь равна %.2f.", width, length, perimeter, area);
     }
 }
