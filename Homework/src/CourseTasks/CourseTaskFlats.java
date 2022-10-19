e837cac67f3097dfc44f20cb3092768e0b5bc6a3
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-19 09:44:45 +0700

    updated CourseTaskFlats.java

--- Homework/src/CourseTasks/CourseTaskFlats.java
+++ Homework/src/CourseTasks/CourseTaskFlats.java
@@ -17,41 +17,45 @@ package CourseTasks;
 
 import java.util.Scanner;
 
-import static java.lang.Math.*;
-
 public class CourseTaskFlats {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите количество этажей в доме:");
-        int floors = scanner.nextInt();
-        System.out.println("Введите количество подъездов в доме: ");
-        int entrances = scanner.nextInt();
-        System.out.println("Введите номер искомой квартиры: ");
-        int flat = scanner.nextInt();
 
-        int flatsOnFloor = 4;
+        int floorsQuantity = scanner.nextInt();
 
-        int flatsTotal = floors * entrances * flatsOnFloor;
-        System.out.println("Всего квартир в доме: " + flatsTotal);
+        System.out.println("Введите количество подъездов в доме:");
 
-        if (flat > flatsTotal || flat < 1) {
-            System.out.printf("Неправильный номер квартиры. Он не может быть больше %d и меньше единицы. Выберите существующий номер квартиры", flatsTotal);
-            System.exit(0);
+        int entrancesQuantity = scanner.nextInt();
+
+        System.out.println("Введите номер искомой квартиры:");
+
+        int flatNumber = scanner.nextInt();
+
+        int flatsQuantityOnFloor = 4;
+
+        int flatsQuantity = floorsQuantity * entrancesQuantity * flatsQuantityOnFloor;
+        System.out.println("Всего квартир в доме:" + flatsQuantity);
+
+        if (flatNumber < 1 || flatNumber > flatsQuantity) {
+            System.out.printf("Неправильный номер квартиры. Он не может быть больше %d и меньше единицы. Выберите существующий номер квартиры", flatsQuantity);
+            return;
         }
 
-        double entrance = ceil((double) flat / (flatsOnFloor * floors));
-        double floor = ceil((flat - (entrance - 1) * floors * flatsOnFloor) / flatsOnFloor);
-        int flatNumberOnFloor = flat % flatsOnFloor;
+        int entranceNumber = (int) Math.ceil((double) flatNumber / (flatsQuantityOnFloor * floorsQuantity));
+        int floorNumber = (int) Math.ceil(((double) flatNumber - (entranceNumber - 1) * floorsQuantity * flatsQuantityOnFloor) / flatsQuantityOnFloor);
+        int flatNumberOnFloor = flatNumber % flatsQuantityOnFloor;
 
-        String flatDescription;
+        String flatLocationOnFloor;
+
         switch (flatNumberOnFloor) {
-            case 1 -> flatDescription = "ближняя слева";
-            case 2 -> flatDescription = "дальняя слева";
-            case 3 -> flatDescription = "дальняя справа";
-            case 0 -> flatDescription = "ближняя справа";
-            default -> flatDescription = "Ошибка! Неправильное количество квартир на площадке!";
+            case 1 -> flatLocationOnFloor = "ближняя слева";
+            case 2 -> flatLocationOnFloor = "дальняя слева";
+            case 3 -> flatLocationOnFloor = "дальняя справа";
+            default -> flatLocationOnFloor = "ближняя справа";
         }
-        System.out.printf("Квартира %d находится в %.0f подъезде на %.0f этаже. Квартира %s.", flat, entrance, floor, flatDescription);
+
+        System.out.printf("Квартира %d находится в %d подъезде на %d этаже. Квартира %s.", flatNumber, entranceNumber, floorNumber, flatLocationOnFloor);
     }
 }
