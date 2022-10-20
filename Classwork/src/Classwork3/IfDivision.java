8cf546e4196ac0184fc7ef97a1ac0371f512b771
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-20 14:21:18 +0700

    updated Classwork 3

--- Classwork/src/Classwork3/IfDivision.java
+++ Classwork/src/Classwork3/IfDivision.java
@@ -6,7 +6,7 @@ public class IfDivision {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите целое число: ");
+        System.out.println("Введите целое число:");
         int number = scanner.nextInt();
 
         if (number % 2 == 0) {
--- Classwork/src/Classwork3/IfPositiveOrNegative.java
+++ Classwork/src/Classwork3/IfPositiveOrNegative.java
@@ -6,7 +6,7 @@ public class IfPositiveOrNegative {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите целое число: ");
+        System.out.println("Введите целое число:");
         int number = scanner.nextInt();
 
         if (number > 0) {
