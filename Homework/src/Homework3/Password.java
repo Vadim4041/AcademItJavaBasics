1dd76537c439eb3f35e80dfa1fcd9291f493f900
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-20 15:37:14 +0700

    updated Password.java

--- Homework/src/Homework3/Password.java
+++ Homework/src/Homework3/Password.java
@@ -23,13 +23,13 @@ public class Password {
         String password = "qwerty123";
 
         System.out.println("Введите пароль:");
-        String passwordInput = scanner.nextLine();
+        String inputPassword = scanner.nextLine();
 
-        if (passwordInput.equals(password)) {
+        if (inputPassword.equals(password)) {
             System.out.println("Вы ввели верный пароль!");
-        } else if (passwordInput.length() > password.length()) {
+        } else if (inputPassword.length() > password.length()) {
             System.out.println("Неверный пароль и слишком длинный");
-        } else if (passwordInput.length() < password.length()) {
+        } else if (inputPassword.length() < password.length()) {
             System.out.println("Неверный пароль и слишком короткий");
         } else {
             System.out.println("Неверный пароль");
