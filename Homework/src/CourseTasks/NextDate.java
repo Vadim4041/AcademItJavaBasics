45406683cae105b920bb9dc1fbe9468ccc7f3f74
Vadim4041 <kozhevnikov404@yandex.ru>
2022-10-20 18:01:04 +0700

    added NextDate.java

---
+++ Homework/src/CourseTasks/NextDate.java
@@ -0,0 +1,22 @@
+package CourseTasks;
+
+/*
+• Программа запрашивает сегодняшнюю дату и выдает дату
+следующего дня
+• Например, входные данные: 31 12 2015, на выходе:
+01.01.2016
+• День, месяц и год можно запрашивать у пользователя с
+консоли по очереди
+• Еще сделать проверку даты на корректность
+ */
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class NextDate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
