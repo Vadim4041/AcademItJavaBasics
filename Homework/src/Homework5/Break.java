f472d8db7deba35fe5d324ae187f6c480ab0f468
Vadim4041 <kozhevnikov404@yandex.ru>
2022-11-02 20:54:07 +0700

    added homework5 Break.java

---
+++ Homework/src/Homework5/Break.java
@@ -0,0 +1,39 @@
+package Homework5;
+
+/*
+• В программе должна быть некоторая загаданная
+фиксированная строка
+• Далее программа предлагает пользователю ввести строку,
+пользователь вводит
+• Если введена та загаданная строка, то программа должна
+завершаться
+• Иначе пользователю дается следующая попытка для ввода
+и т.д., пока не введет правильно. При этом каждый раз
+пользователю должно выдаваться приглашение для ввода
+• В этой задаче используйте бесконечный цикл и break
+ */
+
+import java.util.Scanner;
+
+public class Break {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String correctLine = "123";
+
+        System.out.println("Введите строку:");
+        String inputLine;
+
+        while (true) {
+            inputLine = scanner.nextLine();
+
+            if (inputLine.equals(correctLine)) {
+                break;
+            }
+
+            System.out.println("Неправильно. Попробуйте еще раз:");
+        }
+
+        System.out.println("Поздравляем! Вы ввели правильную строку: " + inputLine);
+    }
+}
