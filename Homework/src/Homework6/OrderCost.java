39a94f0b0dc36d271b6987ad65ca38f77fbf5f16
Vadim4041 <kozhevnikov404@yandex.ru>
2022-11-08 17:13:32 +0700

    updated courseTask OrderCost.java

--- Homework/src/Homework6/OrderCost.java
+++ Homework/src/Homework6/OrderCost.java
@@ -15,16 +15,27 @@ package Homework6;
 
 public class OrderCost {
     public static void main(String[] args) {
-        System.out.println("Общая сумма заказа #1 с учетом скидок: " + getOrderCost(1, 5, 500));
-        System.out.println("Общая сумма заказа #2 с учетом скидок: " + getOrderCost(5, 5, 500));
-        System.out.println("Общая сумма заказа #3 с учетом скидок: " + getOrderCost(5, 5, 1000));
+        System.out.println("Общая сумма заказа #1 с учетом скидок: " + getOrderCost(1, 100, 2, 200));
+        System.out.println("Общая сумма заказа #2 с учетом скидок: " + getOrderCost(5, 50, 5, 20));
+        System.out.println("Общая сумма заказа #3 с учетом скидок: " + getOrderCost(5, 100, 5,500));
     }
 
-    public static double getOrderCost(int type1GoodsQuantity, int type2GoodsQuantity, int totalCost) {
-        if (type1GoodsQuantity + type2GoodsQuantity >= 10 && totalCost >= 1000) {
-            return totalCost * 0.9;
-        } else if (type1GoodsQuantity + type2GoodsQuantity >= 10 || totalCost >= 1000) {
-            return totalCost * 0.95;
+    public static double getOrderCost(int type1ProductsQuantity, double type1ProductCost, int type2ProductsQuantity, double type2ProductCost) {
+        final double discount10Percent = 0.9;
+        final double discount5Percent = 0.95;
+
+        final int quantityThreshold = 10;
+        final int totalCostThreshold = 1000;
+
+        double totalCost = type1ProductsQuantity * type1ProductCost + type2ProductsQuantity * type2ProductCost;
+        int totalQuantity = type1ProductsQuantity + type2ProductsQuantity;
+
+        if (totalQuantity >= quantityThreshold && totalCost >= totalCostThreshold) {
+            return totalCost * discount10Percent;
+        }
+
+        if (totalQuantity >= quantityThreshold || totalCost >= totalCostThreshold) {
+            return totalCost * discount5Percent;
         }
 
         return totalCost;
