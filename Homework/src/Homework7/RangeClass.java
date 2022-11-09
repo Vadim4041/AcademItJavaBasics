d40f228f147d86787afb3dc4476568d2c7776590
Vadim4041 <kozhevnikov404@yandex.ru>
2022-11-09 22:07:25 +0700

    added homework7 Range

---
+++ Homework/src/Homework7/RangeClass.java
@@ -0,0 +1,50 @@
+package Homework7;
+
+/*
+• Создать класс Range (непрерывный вещественный числовой
+диапазон на прямой). В нём:
+1. Объявить два вещественных поля from, to
+2. Описать конструктор, при помощи которого заполняются
+поля
+3. Реализовать геттеры и сеттеры для полей
+4. Сделать метод для получения длины диапазона
+5. Сделать метод isInside, который принимает
+вещественное число и возвращает boolean – результат
+проверки, принадлежит ли число диапазону
+• После этого написать небольшую программу с
+использованием этого класса
+ */
+
+public class RangeClass {
+    private double from;
+    private double to;
+
+    public RangeClass(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getRange() {
+        return to - from;
+    }
+
+    public boolean isInside(double numberToCheck) {
+        return numberToCheck >= from && numberToCheck <= to;
+    }
+}
---
+++ Homework/src/Homework7/RangeMain.java
@@ -0,0 +1,28 @@
+package Homework7;
+
+import java.util.Scanner;
+
+public class RangeMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        RangeClass range = new RangeClass(10, 30);
+
+        System.out.println("Введите начальное значение диапазона:");
+        range.setFrom(scanner.nextInt());
+        System.out.println("Начальное значение диапазона: " + range.getFrom());
+
+        System.out.println("Введите конечное значение диапазона:");
+        range.setTo(scanner.nextInt());
+        System.out.println("Начальное конечное диапазона: " + range.getTo());
+
+        System.out.println("Длина диапазона: " + range.getRange());
+
+        System.out.println("Введите число для проверки вхождения в заданный диапазон:");
+
+        if (range.isInside(scanner.nextInt())) {
+            System.out.println("Число входит в диапазон");
+        } else {
+            System.out.println("Число не входит в диапазон");
+        }
+    }
+}
