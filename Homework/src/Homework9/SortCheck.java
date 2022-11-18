a01b99702625ea94608351493beabeca82888d2e
Vadim4041 <kozhevnikov404@yandex.ru>
2022-11-18 09:02:21 +0700

    updated homework 9 task SortCheck.java

--- Homework/src/Homework9/SortCheck.java
+++ Homework/src/Homework9/SortCheck.java
@@ -11,11 +11,11 @@ public class SortCheck {
     public static void main(String[] args) {
         int[] array = {5, 4, 3, 3, 2};
 
-        System.out.println(isAscending(array));
-        System.out.println(isDescending(array));
+        System.out.println(isSortedInAscendingOrder(array));
+        System.out.println(isSortedInDescendingOrder(array));
     }
 
-    public static boolean isAscending(int[] array) {
+    public static boolean isSortedInAscendingOrder(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i + 1] < array[i]) {
                 return false;
@@ -25,7 +25,7 @@ public class SortCheck {
         return true;
     }
 
-    public static boolean isDescending(int[] array) {
+    public static boolean isSortedInDescendingOrder(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i + 1] > array[i]) {
                 return false;
