f84e1929411a4cdf38d9b72d30101a3798521ce7
Vadim4041 <kozhevnikov404@yandex.ru>
2022-11-24 16:44:02 +0700

    updated course task SelectionSort.java

--- Homework/src/CourseTasks/SelectionSort.java
+++ Homework/src/CourseTasks/SelectionSort.java
@@ -1,24 +1,16 @@
 package CourseTasks;
 
-/*
-• Реализовать функцию поиска минимума в массиве
-• Переделать на функцию, которая ищет индекс, по
-которому лежит минимум в массиве
-• Переделать, чтобы функция поиска индекса
-минимума работала не по всему массиву, а только
-в части массива, начинающейся с индекса start
- */
-
 import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
         int[] array = {100, 55, 99, 22, 45, 78};
 
-        System.out.println(Arrays.toString(sort(array)));
+        sort(array);
+        System.out.println(Arrays.toString(array));
     }
 
-    public static int[] sort(int[] array) {
+    public static void sort(int[] array) {
         for (int j = 0; j < array.length; j++) {
             int minNumberIndex = j;
             int min = array[minNumberIndex];
@@ -34,7 +26,5 @@ public class SelectionSort {
             array[j] = array[minNumberIndex];
             array[minNumberIndex] = temp;
         }
-
-        return array;
     }
 }
