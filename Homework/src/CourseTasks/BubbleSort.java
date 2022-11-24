4f0fe26299eee4aecb3bdcbc15ffbc41dfc33ad2
Vadim4041 <kozhevnikov404@yandex.ru>
2022-11-24 15:04:12 +0700

    added course task BubbleSort.java

---
+++ Homework/src/CourseTasks/BubbleSort.java
@@ -0,0 +1,33 @@
+package CourseTasks;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {100, 55, 99, 22, 45, 78};
+
+        System.out.println(Arrays.toString(sort(array)));
+    }
+
+    public static int[] sort(int[] array) {
+        for (int j = 0; j < array.length; j++) {
+            boolean isNotSwitched = true;
+
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+
+                    isNotSwitched = false;
+                }
+            }
+
+            if (isNotSwitched) {
+                return array;
+            }
+        }
+
+        return array;
+    }
+}
