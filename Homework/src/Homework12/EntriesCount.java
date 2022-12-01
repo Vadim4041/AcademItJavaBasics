772273c3bb2ccff9df0772a46bdce3a4f3a6dc1f
Vadim4041 <kozhevnikov404@yandex.ru>
2022-12-01 10:05:38 +0700

    updated homework 12 task EntriesCount

--- Homework/src/Homework12/EntriesCount.java
+++ Homework/src/Homework12/EntriesCount.java
@@ -18,20 +18,25 @@ public class EntriesCount {
 
         try (Scanner scanner = new Scanner(new FileInputStream("Homework/src/Homework12/EntriesCountInput.txt"))) {
             while (scanner.hasNextLine()) {
-                entriesCount += stringEntriesPerLineCount(scanner.nextLine(), "abc");
+                entriesCount += countSubstringsPerLine(scanner.nextLine(), "abc");
             }
         }
 
         System.out.println("Количество вхождений заданной строки в файле равно: " + entriesCount);
     }
 
-    public static int stringEntriesPerLineCount(String string, String entry) {
+    public static int countSubstringsPerLine(String string, String substring) {
+        String stringInLowerCase = string.toLowerCase();
+        String substringInLowerCase = substring.toLowerCase();
         int count = 0;
-        int offset = 0;
+        int startFrom = 0;
+        int substringIndex = stringInLowerCase.indexOf(substringInLowerCase, startFrom);
+        int substringLength = substringInLowerCase.length();
 
-        while (string.toLowerCase().indexOf(entry, offset) != -1) {
+        while (substringIndex != -1) {
             count++;
-            offset = string.toLowerCase().indexOf(entry, offset) + 1;
+            startFrom = substringIndex + substringLength;
+            substringIndex = stringInLowerCase.indexOf(substringInLowerCase, startFrom);
         }
 
         return count;
